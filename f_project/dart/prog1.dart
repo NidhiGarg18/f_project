2925b08fd5ca84c49d951a05d2489b2c2c796c40
NidhiGarg18 <112054725+NidhiGarg18@users.noreply.github.com>
2024-05-24 14:30:52 +0530

    Add files via upload

---
+++ f_project/dart/prog1.dart
@@ -0,0 +1,14 @@
+class flutter
+{
+    int a=5;   //instance variable
+    void display()
+    {
+        int b=10; //local variable
+        print(a);
+    }
+}
+void main()
+{
+   flutter obj=new flutter();
+   obj.display();
+}
---
+++ f_project/dart/tempCodeRunnerFile.dart
@@ -0,0 +1,6 @@
+
+void main()
+{
+   flutter obj=new flutter();
+   obj.display();
+}
